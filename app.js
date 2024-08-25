06cda80264e97d3f18d6b1b741756981d8fddece
Nadeem Hussain <hoteleyenadeem@gmail.com>
2024-08-25 06:16:44 +0500

    JS assignment 1 & 2 complete

--- app.js
+++ app.js
@@ -1,52 +1,55 @@
 // Chapter No # 01 Alert
-
 // assignment no = 1 
+
+// Question no = 1
 alert("Error ! Please enter a valid password")
 
-// assignment no = 2
+// Question no = 2
 alert("Welcome to JS Land...\nHappy Coding!")
 
-// assignment no = 3
+// Question no = 3
 alert("Welcome to JS Land...")
 
-// assignment no = 4
+// Question no = 4
 alert("Happy Coding! \nPrevent this page from creating additional dialogs.")
 
-// assignment no = 5
+// Question no = 5
 console.log("Hello... I can run JS through my web browser's console")
 
-// Chapter No # 02 Variables for string
 
-// assignment no = 1
+// Chapter No # 02 Variables for string
+// assignment no = 2
+
+// Question no = 1
 var username;
 
-// assignment no = 2
+// Question no = 2
 var myName = "Nadeem Hussain";
 
-// assignment no = 3
+// Question no = 3
 var message;
 
 message = "Hello World";
 
-// assignment no = 4
+// Question no = 4
 var studentBio = "Name: Nadeem Hussain";
 studentBio += "\nAge: 22";
 studentBio += "\nCourse: Certified Mobile Application Development";
 alert(studentBio);
 
-// assignment no = 5
+// Question no = 5
 var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
 alert(pizza);
 
-// assignment no = 6
+// Question no = 6
 var email = "hoteleyenadeem@gmail.com";
 alert("My email address is " + email);
 
-// assignment no = 7
+// Question no = 7
 var book = "A smarter way to learn JavaScript";
 alert("I am trying to learn from the Book " + book);
 
-// assignment no = 8
+// Question no = 8
 document.write("Yah! I can write HTML content through JavaScript");
 alert("Yah! I can write HTML content through JavaScript");
 document.write("<br>");
